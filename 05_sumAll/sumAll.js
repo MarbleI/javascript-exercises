2f63a61807830d7d0e1a26373607f00ab26b2c34
MarbleI <powerdragon04@gmail.com>
2022-07-01 15:04:28 -0500

    added functionality to sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    let value = 0;
+    let test = arg1 - arg2;
+    let test2 = arg1 * arg2;
+    if (test < 0 && test2 > 0) {
+        for (let i = arg1; i <= arg2; i++) {
+            value += i;
+        }
+    } else if (test > 0) {
+        for (let i = arg1; i >= arg2; i--) {
+            value += i;
+        }
+    } else {
+        return "ERROR"
+    }
+    return value
 };
 
 // Do not edit below this line
