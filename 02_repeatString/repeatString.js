57538f90a01cc6abaedfb8072f648e5bb1fcb248
MarbleI <powerdragon04@gmail.com>
2022-07-01 12:24:45 -0500

    finished exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let strings = ''
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (num; num > 0; num--) {
+        strings = strings + string;
+    }
+    return strings;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
