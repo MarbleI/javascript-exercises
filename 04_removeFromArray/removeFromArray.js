c1921124a2c18139664a7458d00630584721e149
MarbleI <powerdragon04@gmail.com>
2022-07-01 14:42:59 -0500

    made functionality for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rmv) {
 
+    for (let i = rmv.length; i >= 0; i--) {
+        let index = array.indexOf(rmv[i]);
+        if (index!== -1) {
+            array.splice(index, 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
