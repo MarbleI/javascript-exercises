26cf86962dda80445e8d56b2f6e1dbd93ae7a44c
MarbleI <powerdragon04@gmail.com>
2022-07-01 15:19:42 -0500

    added functionality to leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(arg1) {
+    if ((arg1 % 400 === 0) || !(arg1 % 100 === 0) && (arg1 % 4 === 0)){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
